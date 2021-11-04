90651f91b197666995464a78dfe3db3e08592f19
aronunlabeled <xaron97x@hotmail.com>
2021-11-03 22:16:14 -0400

    first

---
+++ AronGibsonOMDBApiLab/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using AronGibsonOMDBApiLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AronGibsonOMDBApiLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MovieNightSearch()
+        {
+            return View();
+        }
+
+        public IActionResult MovieNightResult(string t1,string t2,string t3)
+        {
+            MovieModel m1 = movieDAL.GetMovie(t1);
+            MovieModel m2 = movieDAL.GetMovie(t2);
+            MovieModel m3 = movieDAL.GetMovie(t3);
+            List<MovieModel> MovieList = new List<MovieModel> {m1,m2,m3 };
+            return View(MovieList);
+        }
+
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+        public IActionResult MovieResult(string t)
+        {
+            MovieModel MM = movieDAL.GetMovie(t);
+            return View(MM);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AronGibsonOMDBApiLab/Models/MovieDal.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AronGibsonOMDBApiLab.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string t) {
+            string url = $"http://www.omdbapi.com/?apikey=7caabe36&t={t}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            MovieModel movie = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return movie;
+        }
+    }
+}
